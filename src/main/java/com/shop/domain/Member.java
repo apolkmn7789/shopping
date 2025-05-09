09be16af6dda47a86fff58cad7cb11dc95d86a98
apolkmn7789 <ip01021107076@gmail.com>
2025-05-09 14:39:30 +0900

    admin signup

--- src/main/java/com/shop/domain/Member.java
+++ src/main/java/com/shop/domain/Member.java
@@ -40,7 +40,6 @@ public class Member {
         member.setAddress(memberFormDto.getAddress());
         String password = passwordEncoder.encode(memberFormDto.getPassword());
         member.setPassword(password);
-        member.setRole(Role.USER);
         return member;
     }
 }
--- src/main/java/com/shop/member/MemberController.java
+++ src/main/java/com/shop/member/MemberController.java
@@ -11,19 +11,19 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
+
 public class MemberController {
 
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
-    // 회원 가입 url
     @GetMapping(value = "/new")
     public String memberForm(Model model){
-        model.addAttribute("memberFormDto",new MemberFormDto());
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        model.addAttribute("actionUrl", "/members/new");
         return "member/memberForm";
     }
 
@@ -32,28 +32,56 @@ public class MemberController {
                              BindingResult bindingResult,
                              Model model){
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "member/memberForm";
         }
 
         try {
-        Member member = Member.createMember(memberFormDto , passwordEncoder);
-        memberService.saveMember(member);
-        }catch (IllegalStateException e){
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            member.setRole(Role.USER);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
             model.addAttribute("errorMessage", e.getMessage());
             return "member/memberForm";
         }
         return "redirect:/";
     }
-    // 로그인 페이지
+
+    @GetMapping(value = "/admin/new")
+    public String adminForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        model.addAttribute("actionUrl", "/members/admin/new");
+        return "member/memberForm";
+    }
+
+    @PostMapping(value = "/admin/new")
+    public String adminForm(@Valid MemberFormDto memberFormDto,
+                            BindingResult bindingResult,
+                            Model model){
+
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            member.setRole(Role.ADMIN);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+        return "redirect:/";
+    }
+
     @GetMapping(value = "/login")
     public String loginMember(){
         return "/member/memberLoginForm";
     }
-    // 로그인 실패 페이지
+
     @GetMapping(value = "/login/error")
     public String loginError(Model model){
-        model.addAttribute("loginErrorMsg","아이디 또는 비밀번호를 입력해주세요");
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 입력해주세요");
         return "/member/memberLoginForm";
     }
 }
